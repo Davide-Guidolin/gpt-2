b58cdb578d9f3fc69458de55ce104fcb4e6b8716
Dagu9 <davide.guidolin@studenti.unitn.it>
2024-06-16 18:59:11 +0200

    Inference using weights from hf

--- gpt2.py
+++ gpt2.py
@@ -3,6 +3,8 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+from config import GPTConfig
+
 class CausalSelfAttention(nn.Module):
     
     def __init__(self, config):
@@ -17,7 +19,7 @@ class CausalSelfAttention(nn.Module):
         self.n_head = config.n_head
         self.n_embd = config.n_embd
         
-        # mask
+        # autoregressive mask - used to attend only to past tokens 
         self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
                              .view(1, 1, config.block_size, config.block_size))
         
@@ -69,6 +71,8 @@ class Block(nn.Module):
     def forward (self, x):
         x = x + self.attn(self.ln_1(x))
         x = x + self.mlp(self.ln_2(x))
+        
+        return x
 
 
 class GPT(nn.Module):
@@ -84,3 +88,71 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embd),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+        
+    def forward(self, idx):
+        B, T = idx.size()
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, maximum block size is {self.config.block_size}"
+        
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device)
+        pos_emb = self.transformer.wpe(pos)
+        tok_emb = self.transformer.wte(idx)
+        
+        x = tok_emb + pos_emb
+        
+        for block in self.transformer.h:
+            x = block(x)
+            
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x)
+        
+        return logits        
+        
+    @classmethod
+    def from_pretrained(cls, model_type):
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        
+        from transformers import GPT2LMHeadModel
+        print(f"Loading weights from pretrained gpt: {model_type}")
+        
+        config_args = {
+            'gpt2' :        dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+            'gpt2-medium' : dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
+            'gpt2-large' :  dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
+            'gpt2-xl' :     dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
+        }[model_type]
+        
+        config_args['vocab_size'] = 50257
+        config_args['block_size'] = 1024
+        
+        # This model
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')]
+        
+        # HF model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+        
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')]
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')]
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        
+        assert len(sd_keys_hf) == len(sd_keys), f"Mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+                    
+            else:
+                assert sd_hf[k].shape == sd[k].shape, f"Mismatch {k}: {sd_hf[k].shape} != {sd[k].shape}"
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+        
+        return model
+                    
+        
