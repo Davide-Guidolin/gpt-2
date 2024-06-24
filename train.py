67927ffb455fa3464106488db5d8966a9e79467c
Dagu9 <davide.guidolin@studenti.unitn.it>
2024-06-24 14:49:58 +0200

    Measure time

--- train.py
+++ train.py
@@ -1,17 +1,18 @@
 import torch
+import time
 
 from gpt2 import GPT
 from config import GPTConfig
 from data import DataLoaderLite
 
+# constants
+B = 4   # batch size
+T = 32  # tokens per sample
+
 # seed
 torch.manual_seed(1337)
 if torch.cuda.is_available():
     torch.cuda.manual_seed(1337)
-
-# constants
-B = 4   # batch size
-T = 32  # tokens per sample
 
 # detect available devices
 device = "cpu"
@@ -32,6 +33,7 @@ model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
 
 for i in range(50):
+    t0 = time.time()
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
@@ -40,6 +42,9 @@ for i in range(50):
     # backward and step
     loss.backward()
     optimizer.step()
-    
-    print(f"step {i}, loss: {loss.item()}")
+    torch.cuda.synchronize()
+    t1 = time.time()
+    dt = (t1 - t0) * 1000 # time difference in ms
+    tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
+    print(f"step {i}, loss: {loss.item()}, dt: {dt:.2f}ms, tok/sec: {tokens_per_sec:.2f}")
     
